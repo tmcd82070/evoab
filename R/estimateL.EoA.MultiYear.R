8a8af6f21ea6cad804213c93f0f07abb0214bbcd
Trent <tmcdonald@west-inc.com>
2017-03-08 18:06:08 -0700

    Commit of estimateL.EoA.Multiyear that includes covariates in "vague" prior option
    Initial commit of convergence checking

--- R/estimateL.EoA.MultiYear.R
+++ R/estimateL.EoA.MultiYear.R
@@ -10,8 +10,8 @@
 #' or an informed prior.
 #' Estimation is performed using JAGS.
 #'
-#' @param X Vector of number of carcasses found, one element per year. If
-#' multiple sites are involved, elements of X are the total (summed) number
+#' @param Y Vector of number of carcasses found, one element per year. If
+#' multiple sites are involved, elements of Y are the total (summed) number
 #' of targets found per season.
 #'
 #' @param beta.params A data frame containing, at a minimum, two columns named \code{$alpha}
@@ -44,22 +44,41 @@
 #'    alpha = c( 69.9299, 63.5035,  84.6997),
 #'    beta = c(  736.4795,  318.3179, 759.9333 )
 #'    )
-#' X <- c( 0, 1, 3)
+#' Y <- c( 0, 1, 3)
 #'
 #'
-#' eoa <- estimateL.EoA.MultiYear( X, g, LMax=500 )  # Un-informed EoA
+#' eoa <- estimateL.EoA.MultiYear( Y, g, LMax=500 )  # Un-informed EoA
 #'
-#' ieoa <- estimateL.EoA.MultiYear( X, g, Lprior="normal", Lprior.mean=20, Lprior.sd=4) # Informed EoA
+#' ieoa <- estimateL.EoA.MultiYear( Y, g, Lprior="normal", Lprior.mean=20, Lprior.sd=4) # Informed EoA
 #'
 #'
-#' # To check convergence, run traceplot and Gelman stats
-#' plot(ieoa$out) # tracePlot
+#' # Check convergence and autocorrelation
+#'
 #' gelman.diag(ieoa$out) # gelmanStats
 #' gelman.plot(ieoa$out) # gelmanPlot
 #'
-estimateL.EoA.MultiYear <- function(lambda, X, beta.params, data, Lprior="uniform",
-                                    Lprior.mean=NULL, Lprior.sd=NULL, LMax=62,
-                                    conf.level=0.9 ){
+#' # Traceplots
+#' library(lattice)
+#' plot(ieoa$out) # tracePlot, all parameters
+#' xyplot(ieoa$out[,grep("^a\\[",varnames(ieoa$out))]) # nicer trace of coefficients
+#'
+#' # Autocorrelation functions
+#' acfplot(ieoa$out[,grep("^a\\[",varnames(ieoa$out))], ylim=c(-.2,1), lag.max=300)
+#' acfplot(ieoa$out[,grep("^a\\[",varnames(ieoa$out))], ylim=c(-.2,1), thin=10)
+#'
+#' # Density plots
+#' densityplot(ieoa$out[,grep("^a\\[",varnames(ieoa$out))])
+#'
+#' # Correlation among coefficients
+#' levelplot(ieoa$out[,grep("^a\\[",varnames(ieoa$out))][[1]])
+#'
+#' # QQ plots
+#' qqmath(ieoa$out[,grep("^a\\[",varnames(ieoa$out))])
+#'
+estimateL.EoA.MultiYear <- function(lambda, beta.params, data, Lprior="vague",
+                                    Lprior.mean=NULL, Lprior.sd=NULL,
+                                    conf.level=0.9, nburns = 500000, niters = 20000,
+                                    nthins = 10 ){
 	library(rjags)
 
   # Resolve formula for lambda
@@ -73,10 +92,12 @@ estimateL.EoA.MultiYear <- function(lambda, X, beta.params, data, Lprior="unifor
   mf[[1L]] <- quote(stats::model.frame)
   mf <- eval(mf, parent.frame())
   mt <- attr(mf, "terms")
-  lambda.covars <- if (!is.empty.model(mt))
+  Y <- model.response(mf,"any")
+  lambda.covars <- if (!is.empty.model(mt)){
     model.matrix(mt, mf, contrasts)
+  }
 
-	nyrs <- length(X)
+	nyrs <- length(Y)
 	if( length(beta.params$alpha) == 1 ){
 		alpha.vec <- rep(beta.params$alpha, nyrs)
 		beta.vec <- rep(beta.params$beta, nyrs)
@@ -85,22 +106,28 @@ estimateL.EoA.MultiYear <- function(lambda, X, beta.params, data, Lprior="unifor
 		beta.vec <- beta.params$beta
 	}
 
-	if( length(alpha.vec) != nyrs ) stop("Length of alpha and beta vectors must be 1 or equal length of X")
+	if( length(alpha.vec) != nyrs ) stop("Length of alpha and beta vectors must be 1 or equal length of Y")
 	if( length(alpha.vec) != length(beta.vec) ) stop("Lengths of alpha and beta inputs must be equal") # Actually, this can't happen if beta.params is a data frame. Oh well, leave it.
 
+
 	## ---- bayesModelCode ----
-	if( Lprior == "uniform" ){
+	if( Lprior == "vague" ){
+	  # Recall: tau of dnorm in JAGS is 1/variance
+	  coefTaus <- compVagueSd(Y,alpha.vec,beta.vec,lambda.covars, range.multiplier = 100)
+	  print(coefTaus)
+	  coefTaus <- 1/coefTaus^2
+
 		jagsModel <- "model{
 
 		# Priors
     for(i in 1:ncovars){
-      a[i] ~ dnorm( 0, 4)
+      a[i] ~ dnorm( 0, coefTaus[i] )
     }
 
     # functional relations
     for(i in 1:nx){
       for(j in 1:ncovars){
-        logl[i,j] <- a[i]*lambda.covars[i,j]
+        logl[i,j] <- a[j]*lambda.covars[i,j]
       }
       lambda[i] <- exp(sum(logl[i,]))
     }
@@ -109,7 +136,7 @@ estimateL.EoA.MultiYear <- function(lambda, X, beta.params, data, Lprior="unifor
 		for( i in 1:nx ){
 			g[i] ~ dbeta(alpha[i], beta[i])
 			M[i] ~ dpois(lambda[i])
-			X[i] ~ dbin( g[i], M[i] )
+			Y[i] ~ dbin( g[i], M[i] )
 		}
 
 		Mtot <- sum(M[])
@@ -117,12 +144,12 @@ estimateL.EoA.MultiYear <- function(lambda, X, beta.params, data, Lprior="unifor
 		}
 		"
 
-		JAGS.data.0 <- list ( X = X,
+		JAGS.data.0 <- list ( Y = Y,
 													nx = nyrs,
 													ncovars = ncol(lambda.covars),
+													coefTaus = coefTaus,
 													alpha = alpha.vec,
 													beta = beta.vec,
-													lambdaMax = LMax,
 													lambda.covars = lambda.covars)
 
 } else if (Lprior == "normal"){
@@ -146,7 +173,7 @@ estimateL.EoA.MultiYear <- function(lambda, X, beta.params, data, Lprior="unifor
 	for( i in 1:nx ){
 		g[i] ~ dbeta(alpha[i], beta[i])
 		M[i] ~ dpois(lambda[i])
-		X[i] ~ dbin( g[i], M[i] )
+		Y[i] ~ dbin( g[i], M[i] )
 	}
 
 	Mtot <- sum(M[])
@@ -154,7 +181,7 @@ estimateL.EoA.MultiYear <- function(lambda, X, beta.params, data, Lprior="unifor
 	}
 	"
 
-	JAGS.data.0 <- list ( X = X,
+	JAGS.data.0 <- list ( Y = Y,
 												nx = nyrs,
 												ncovars = ncol(lambda.covars),
 												alpha = alpha.vec,
@@ -169,14 +196,13 @@ estimateL.EoA.MultiYear <- function(lambda, X, beta.params, data, Lprior="unifor
 writeLines(jagsModel, "model.txt")
 #cat(jagsModel)
 
-print(JAGS.data.0)
 
 ## ---- initialValues ----
 
 Inits <- function(x){
 	if( !is.null(x$lmean) ){
 		gg <-rbeta(x$nx, x$alpha, x$beta)
-		M <- ceiling(x$X / gg) + 1
+		M <- ceiling(x$Y / gg) + 1
 		l <- mean(M)
 		list ( lambda = l,
 					 M = M,
@@ -185,7 +211,7 @@ Inits <- function(x){
 					 .RNG.seed=93229 )
 	} else {
 		gg <-rbeta(x$nx, x$alpha, x$beta)
-		M <- ceiling(x$X / gg) + 1
+		M <- ceiling(x$Y / gg) + 1
 		a <- rep(0,ncol(x$lambda.covars))
 		a[1] <- log(mean(M))
 		list ( a = a,
@@ -202,15 +228,12 @@ inits = list(  Inits(JAGS.data.0),
 
 
 # MCMC sample size settings:
-niters <- 10000
-nthins <- 10
-nburn  <- 75000
 nchains <- 3
 nadapt <- 3000
 
 
 # Parameters to be monitored by WinBUGS
-params <- c("a", "M", "g", "lambda", "Mtot")
+params <- c("a", "M", "g", "lambda", "Mtot","logl")
 
 
 ## ---- jagsRun ----
@@ -225,7 +248,7 @@ jags = jags.model(file="model.txt",
 									n.adapt=nadapt)
 #   Run out in the chain a ways
 cat("Burning...\n")
-out = update(jags, n.iter=nburn)
+out = update(jags, n.iter=nburns)
 
 # Run the MCMC chains
 cat("MCMC Sampling...\n")
@@ -235,10 +258,44 @@ out = coda.samples(jags,
 									 thin=nthins)
 
 (t2=Sys.time())
-print(t2-t1)
+t3 <- t2-t1
+cat(paste("Execution time:", round(t3,2), attr(t3,"units"), "\n\n"))
 
-## ---- mcmcSummary ----
-print(summary(out))
+## ---- mcmcChecking ----
+#print(summary(out))
+out.coefs <- out[,grep("^a\\[",varnames(out))]
+
+# Check convergence
+cat(paste0(paste(rep("-",4),collapse="")," Coefficient convergence:\n"))
+Rhats <- gelman.diag(out.coefs)
+print(Rhats)
+Rhats <- Rhats$psrf[,"Upper C.I."]
+if( any(Rhats > 1.1) ){
+  cat("The following coefficients probably did not converge (Rhat > 1.1):\n")
+  print(names(Rhats)[Rhats > 1.1])
+  cat("Try increasing nburns and/or nthins or informing the coefficient's priors.\n")
+} else {
+  cat("The model converged. All Rhats < 1.1\n")
+}
+
+# Check autocorrelation
+cat(paste0("\n", paste(rep("-",4),collapse="")," Coefficient autocorrelation:\n"))
+vn <- varnames(out.coefs)
+ncovars <- ncol(lambda.covars)
+acflag2 <- rep(NA,ncovars)
+for( i in 1:ncovars ){
+  tmp <- rep(NA,nchains)
+  for(j in 1:nchains){
+    tmp.2 <- acf(as.matrix(out.coefs[[j]][,i]), lag.max = 2, plot=FALSE)
+    tmp[j] <- tmp.2$acf[tmp.2$lag ==2,,]
+  }
+  acflag2[i] <- mean(tmp)
+  cat(paste("Coef.", vn[i], ": mean(acf[lag 2]) =", round(acflag2[i],5), "\n") )
+}
+if(any(acflag2>0.4)){
+  cat("\nSome autocorrelations exceed 0.4. Inspect acfplot() and increase thinning.\n")
+}
+
 
 
 ## ---- quantiles ----
@@ -261,21 +318,44 @@ list(
   #lambda.est=data.frame(lambda=l.50, lambda.sd=l.sd, lambda.lo=l.CI[1], lambda.hi=l.CI[2],
 	#												 Mtot=M.50, Mtot.sd=M.sd, Mtot.lo=M.CI[1], Mtot.hi=M.CI[2],
 	#												 ci.level=conf.level),
-  out=out)
+  out=out,
+  priorCoefSd = 1/sqrt(coefTaus),
+  design.mat=lambda.covars)
 }
 
 
 
 # ---- Testing
 
+# A simple example with three years, four observations
+g <- data.frame(
+  alpha = c( 69.9299, 63.5035,  84.6997, 84.6997),
+  beta = c(  736.4795,  318.3179, 759.9333, 759.9333 )
+  )
+Y <- c( 0, 1, 3, 10 )
+
+tmp.df <- data.frame(year=factor(c("2015","2016","2017","2017")),
+                     Year=c(1,2,3,3))
+
+
+# A more complicated example, with 3 years, 100 observations, lambda slope = 20
+set.seed(9430834)
+n <- 21
+n3 <- round(n/3)
 
 g <- data.frame(
-  alpha = c( 69.9299, 63.5035,  84.6997),
-  beta = c(  736.4795,  318.3179, 759.9333 )
-  )
-X <- c( 0, 1, 3)
+  alpha = rnorm(n,70,2),
+  beta = rnorm(n,700,25)
+)
+Y <- rbinom(n, c(rep(20,n3), rep(40,n3), rep(60,n-2*n3)), g$alpha/(g$alpha+g$beta))
 
-tmp.df <- data.frame(year=factor(c("2015","2016","2017")))
+tmp.df <- data.frame(year=factor(c(rep("2015",n3),rep("2016",n3),rep("2017",n-2*n3))),
+                     Year=c(rep(1,n3),rep(2,n3),rep(3,n-2*n3)))
 
 
-eoa <- estimateL.EoA.MultiYear(~year, X, g, data=tmp.df, LMax=500 )  # Un-informed EoA
+
+
+eoa <- estimateL.EoA.MultiYear(Y~Year, g, data=tmp.df, niters= 2000*10, nthins = 10 )  # Un-informed EoA
+tmp.1 <-apply(as.matrix(eoa$out),2,median)
+
+#print(tmp.1)
